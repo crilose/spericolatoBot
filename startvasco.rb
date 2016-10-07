caad3fec03f30dd11af9ae4c31ffb450822602b8
root <root@cloud.ingstudio.it>
2016-10-07 11:44:30 +0200

    Aggiunti files mancanti

--- startvasco.rb
+++ startvasco.rb
@@ -1,15 +1,3 @@
 require 'daemons'
 require 'rubygems'
-
-current_dir = Dir.pwd
-
-#comando per gestire il demone
-cmd = ARGV[0]
-
-#oggetto che contiene gli argomenti da passare allo script demone
-options = {
-    :ARGV       => ["#{cmd}", '-f', '--', "#{current_dir}/"],
-}
-
-#demonizzazione dello script
-Daemons.run('VascoRossi.rb', options)
+Daemons.run('VascoRossi.rb')
